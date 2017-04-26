cdf4fb2ef65e9f167e6d8420d56cd7dddacfa3af
ginopc <ginopc@tiscali.it>
2017-04-26 15:17:33 +0000

    first working version

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ var PORT = process.env.PORT || 8080;
 // define static pages
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-	res.send('hello world!');
+app.get('/', function(req, res) {
+   res.end('Header Parser microservice - by Maurizio Aru');
+});
+
+app.get('/api/whoami', function(req, res) {
+
+	var result = {
+	   address: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+	   language: req.headers['accept-language'].split(',')[0],
+	   software: req.headers['user-agent'].split('(')[1].split(')')[0],
+	}
+	
+	console.log(req);
+	res.json(result);
 });
 
 app.listen( PORT, function(){
